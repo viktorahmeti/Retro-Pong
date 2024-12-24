3bd64354649511db2a1a736c80a004da1205cf0a
Viktor Ahmeti <viktorahmeti2001@gmail.com>
2024-12-24 14:58:42 +0100

    small fix

--- ball.js
+++ ball.js
@@ -91,10 +91,8 @@ class BouncingBallCanvas extends Canvas {
     }
 
     _drawBall = () => {
-        this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         this.ctx.fill();
-        this.ctx.closePath();
     }
 
     _getRandomVelocities = () => {
